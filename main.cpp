adf721e8f7167fc8f39277ec656dc67d4062f5b1
Reuben <2824016820@qq.com>
2021-07-09 15:10:46 +0800

    main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <unistd.h>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int p1 = fork();
+    if (p1 == 0) {
+        cout << 'B' << endl;
+        return 0;
+    }
+
+    int p2 = fork();
+    if (p2 == 0) {
+        cout << 'C' << endl;
+        return 0;
+    }
+
+    if (p1 > 0 && p2 > 0) {
+        cout << 'A' << endl;
+    }
+
+//    getchar();
     return 0;
 }
